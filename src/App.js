1d7060212a22982f486e79420c54e772c55decd1
pixel-2021 <websupport@pixel-studios.com>
2022-03-01 17:31:28 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './component/Header';
 
 function App() {
     return ( 
-        <div className = "App" >
+        <div className = "App">
         <Header/>
         </div>
     );
